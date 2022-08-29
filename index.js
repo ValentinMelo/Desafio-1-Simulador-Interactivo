fc223c4a473028c8d59db4028f99880980c55f07
Vale Melo <valemelo18@gmail.com>
2022-08-28 22:15:33 -0300

    Se agregan funciones

--- index.js
+++ index.js
@@ -4,20 +4,31 @@ let numeroCuotas = parseInt(prompt('Ingresa la cantidad de cuotas a pagar (entre
 let maximoCapital = 5000
 let maximoCuotas = 12
 
-//tasa de interes para prestamos menores o iguales a 6 cuotas
-let interes1 = 2
-//tasa de interes para prestamos mayores a 6 cuotas
-let interes2 = 5
+if(numeroCuotas<=6){
+   alert('El interés a pagar es del 2% sobre la cuota mensual.')
+} else {
+    alert('El interés a pagar es del 5% sobre la cuota mensual.')
+}
 
-let montoCuota1 = (capital/numeroCuotas)*(1+(interes1/100))
-let montoCuota2 = (capital/numeroCuotas)*(1+(interes2/100))
+
+function montoCuota(prestamo){
+    let interes = 0
+    if(numeroCuotas<=6){
+        interes = 2
+    } else {
+        interes = 5
+    }
+    let valorCuota = (capital/numeroCuotas)*(1+(interes/100))
+    prestamo = valorCuota
+    return prestamo
+}
 
 if (capital<=maximoCapital && numeroCuotas<=6){
     for(let i=1;i<=numeroCuotas;i++){
-            console.log('Cuota ',i,': ',montoCuota1)
+            console.log('Cuota ',i,': ',montoCuota(capital))
     }
 } else if(capital<=maximoCapital && numeroCuotas>6 && numeroCuotas<=maximoCuotas) {
     for(let i=1;i<=numeroCuotas;i++){
-        console.log('Cuota ',i,': ',montoCuota2)
+        console.log('Cuota ',i,': ',montoCuota(capital))
 }
 } else alert ('Requisitos: máximo de capital 5.000 USD y máximo de cuotas 12')
